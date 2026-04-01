3deff2e70e51a70ee961d179a6cfa478703aef7b
yaadevmohit <mohityadav530@gmail.com>
2026-03-31 22:54:20 -0400

    main change

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -13,6 +14,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
