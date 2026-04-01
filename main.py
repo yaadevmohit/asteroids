a9ec607068870afcf04108e5d2610394ac793cbb
yaadevmohit <mohityadav530@gmail.com>
2026-03-31 23:18:30 -0400

    new circleshape added

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-        clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
